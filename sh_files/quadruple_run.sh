78fa76c141911d4f842ddb20d99b9d0234dd5dbd
Sanket Rane <sanketrane@users.noreply.github.com>
2022-11-28 13:02:45 -0500

    Update quadruple_run.sh

--- sh_files/quadruple_run.sh
+++ sh_files/quadruple_run.sh
@@ -7,7 +7,7 @@ do
     esac
 done
 
-for i in 1 2 3 4 5 6
+for i in 1 2 3 4 
     do
       ./stan_models/New_modelsCAR/${modelname} sample num_warmup=500 num_samples=1500 data file=datafiles/Bcell_Imm.Rdump \
       output file=save_csv/${modelname}_${i}.csv &
