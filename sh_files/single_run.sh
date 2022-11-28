cb20f5187b5f4a2f5ed7987f332a6641d385a869
Sanket Rane <sanketrane@users.noreply.github.com>
2022-11-28 12:25:54 -0500

    Update single_run.sh

--- sh_files/single_run.sh
+++ sh_files/single_run.sh
@@ -6,4 +6,4 @@ do
     esac
 done
 
-stan_models/MAP_${modelname} sample num_samples=500 num_warmup=300 data file=data/Treg_data_S33.Rdump output file=save_csv/${modelname}_c1.csv
+./stan_models/New_modelsCAR/${modelname} sample num_warmup=500 num_samples=500 data file=datafiles/Bcell_Imm.Rdump output file=save_csv/${modelname}_${i}.csv
