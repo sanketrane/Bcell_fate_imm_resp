6bc3a53ca218b2ea1aadf3e20883e3b67ab6126e
Sanket <sanketrn@gmail.com>
2023-04-05 13:55:18 -0400

    co-transfer update

--- sh_files/quadruple_run.sh
+++ sh_files/quadruple_run.sh
@@ -7,8 +7,8 @@ do
     esac
 done
 
-for i in 1 2 3 4 
+for i in 1 2 3 4 5 6
     do
-      ./stan_models/${modelname} sample num_warmup=500 num_samples=1500 data file=datafiles/Bcell_Imm.Rdump \
+      ./stan_models/${modelname} sample num_warmup=500 num_samples=2500 data file=datafiles/Bcell_Imm.Rdump \
       output file=save_csv/${modelname}_${i}.csv &
     done
