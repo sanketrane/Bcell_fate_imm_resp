15be382444fb94af2e3b10ad3d4cec993a085b00
Sanket <sanketrn@gmail.com>
2023-04-14 18:10:35 -0400

    response writeup

--- Rscripts/Co-transfer_sim.R
+++ Rscripts/Co-transfer_sim.R
@@ -7,6 +7,32 @@ library(rstan)
 modelName1 <- "Branched_timeinflux1"
 modelName2 <- "Linear_timeinflux1"
 
+theme_set(theme_light())
+myTheme <- theme(text = element_text(size = 12), axis.text = element_text(size = 12), axis.title =  element_text(size = 12, face = "bold"),
+                 plot.title = element_text(size=12,  hjust = 0.5, face = "bold"),
+                 legend.background = element_blank(), legend.key = element_blank(),
+                 legend.text = element_text(size=12), legend.title = element_text(12),
+                 strip.text.x = element_text(size = 14))
+
+
+fancy_scientific <- function(l) {
+  # turn in to character string in scientific notation
+  l <- format(l, scientific = TRUE)
+  # quote the part before the exponent to keep all the digits
+  l <- gsub("^(.*)e", "'\\1'e", l)
+  # remove + after exponent, if exists. E.g.: (e^+2 -> e^2)
+  l <- gsub("e\\+","e",l)  
+  # turn the 'e' into plotmath format
+  l <- gsub("e", "%*%10^", l)
+  # convert 1x10^ or 1.000x10^ -> 10^
+  l <- gsub("\\'1[\\.0]*\\'\\%\\*\\%", "", l)
+  # return this as an expression
+  parse(text=l)
+}
+
+log10minorbreaks=as.numeric(1:10 %o% 10^(1:8))
+
+
 ### extract posterior distribution  of parameters as a df
 fit_ss <- read.csv(file = paste0("PostDF_", modelName1, ".csv"))
 mu_b <- median(fit_ss$mu)
@@ -28,7 +54,7 @@ ode_func <-  function(t, state, parms){
     ## parameters estimated from modeling immunization data
     alpha = parms[1]
     mu_branched = parms[2]
-    mu_linear = parms[3]
+    mu_linear = 0#parms[3]
     delta = parms[4]
     lambda_Branched = parms[5]
     lambda_linear = parms[6]
@@ -73,7 +99,7 @@ ts_pred <- seq(7, 60, length.out=100)
 
 
 out_df <- data.frame()
-eps_vec <- c(0.01, 0.03, 0.1, 0.3, 1, 3)
+eps_vec <- c(0.01, 0.7, 1, 3)
 
 for(i in 1:length(eps_vec)) {
   parms_vec <- c("alpha" = alpha_b, "mu_branched" = mu_b , "mu_linear" = mu_l, 
@@ -90,65 +116,59 @@ for(i in 1:length(eps_vec)) {
            GC.2 = Y4,
            MZ.2 = Y5) %>%
     mutate(ratio_MZ = replace_na(MZ.2/MZ.1, 0),
-           EPS = round(eps_vec[i], 2))
+           EPS = round(eps_vec[i], 4))
   out_df <- rbind(out_df, sol_df)
   out_df
 }
 
+plot_df <- out_df %>% 
+  mutate(FO.1 = replace(FO.1, FO.1 <= 1, 1),
+         GC.1 = replace(GC.1, GC.1 <= 1, 1),
+         MZ.1 = replace(MZ.1, MZ.1 <= 1, 1),
+         GC.2 = replace(GC.2, GC.2 <= 1, 1),
+         MZ.2 = replace(MZ.2, MZ.2 <= 1, 1))
+
+plot_df$EPS <- factor(plot_df$EPS, levels = c("0.01", "0.7", "1", "3"), ordered = TRUE,
+                      labels = c(expression(paste(phi, "=0.01")), expression(paste(phi, "=0.7")),
+                                 expression(paste(phi, "=1")), expression(paste(phi, "=3"))))
 
 
-#ggplot(data = sol_df, aes(x= timeseries)) +
-#  geom_area(aes(y= FO.1 + MZ.1 + GC.2 + MZ.2, fill="FO CD45.1" ), alpha=0.6, colour="white") +
-#  geom_area(aes(y= MZ.1 + GC.2 + MZ.2, fill="MZ CD45.1" ), alpha=0.6, colour="white") +
-#  geom_area(aes(y= GC.2 + MZ.2, fill="GC CD45.2" ), alpha=0.5, colour="white") +
+#ggplot(data = out_df, aes(x= timeseries)) +
+#  geom_area(aes(y= GC.1 + MZ.1 + GC.2 + MZ.2 , fill="GC CD45.1" ), alpha=0.6, colour="white") +
+#  geom_area(aes(y= GC.2 + MZ.2 , fill="GC CD45.2" ), alpha=0.5, colour="white") +
 #  geom_area(aes(y= MZ.2, fill="MZ CD45.2"), alpha=0.5, colour="white") +
-#  #scale_fill_viridis(discrete = T) +
-#  #theme_ipsum() + 
-#  scale_x_log10(limits = c(7, 60), breaks=c(7, 14, 28, 56)) + scale_y_log10() + 
-#  labs(x='Days since immunization', y=NULL)  +  theme_ipsum() +
-#  theme(legend.title = element_blank())
+#  geom_area(aes(y= MZ.1 + GC.2 + MZ.2 , fill="MZ CD45.1" ), alpha=0.6, colour="white") +
+#  scale_x_log10(limits = c(7, 60), breaks=c(7, 14, 28, 56)) + 
+#  scale_y_continuous(limits = c(1, 1e6), trans = "log10", labels = fancy_scientific, minor_breaks = log10minorbreaks) + 
+#  labs(x='Days since immunization', y=NULL)  +  #theme_ipsum() +
+#  guides(fill='none')+
+#  facet_wrap(~EPS, nrow = 3, labeller = as_labeller(plot_labl)) + myTheme
 
-
-plot_labl <- c(`0.1` = "eps=0.1", `0.2` = "eps=0.25", `0.3` = "eps=0.33", `0.5` = "eps=0.5", 
-               `1` = "eps=1", `2` = "eps=2", `3` = "eps=3", `4` = "eps=4", 
-               `5` = "eps=5" )
-
-plot_labl <- c(`0.01` = "eps=0.01", `0.03` = "eps=0.03", `0.1` = "eps=0.1", `0.3` = "eps=0.3", 
-               `1` = "eps=1", `3` = "eps=3")
-
-
-
-ggplot(data = out_df, aes(x= timeseries)) +
-  #geom_area(aes(y= FO.1 + GC.1 + MZ.1 + GC.2 + MZ.2, fill="FO CD45.1" ), alpha=0.6, colour="white") +
-  geom_area(aes(y= GC.1 , fill="GC CD45.1" ), alpha=0.6, colour="white") +
-  geom_area(aes(y= MZ.1 , fill="MZ CD45.1" ), alpha=0.6, colour="white") +
-  geom_area(aes(y= GC.2 , fill="GC CD45.2" ), alpha=0.5, colour="white") +
+ggplot(data = plot_df, aes(x= timeseries)) +
+  geom_area(aes(y= GC.1, fill="GC CD45.1" ), alpha=0.6, colour="white") +
+  geom_area(aes(y= GC.2, fill="GC CD45.2" ), alpha=0.5, colour="white") +
   geom_area(aes(y= MZ.2, fill="MZ CD45.2"), alpha=0.5, colour="white") +
-  #scale_fill_viridis(discrete = T) +
-  scale_x_log10(limits = c(7, 60), breaks=c(7, 14, 28, 56)) + scale_y_log10(limits = c(1, 1e6)) + 
+  geom_area(aes(y= MZ.1, fill="MZ CD45.1" ), alpha=0.6, colour="white") +
+  scale_x_log10(limits = c(7, 60), breaks=c(7, 14, 28, 56)) + 
+  scale_y_continuous(limits = c(1, 2e5), trans = "log10", labels = fancy_scientific, minor_breaks = log10minorbreaks) + 
   labs(x='Days since immunization', y=NULL)  +  #theme_ipsum() +
-  theme(legend.title = element_blank()) +
-  #guides(fill='none')+
-  facet_wrap(~EPS, nrow = 3, labeller = as_labeller(plot_labl)) 
-
-
-
-ggplot(out_df, aes(x=timeseries, y=ratio_MZ))+
-  geom_line(col=4, size=1.25)+ ylim(0, 1)+
-  scale_x_log10(limits = c(7, 60), breaks=c(7, 14, 28, 56)) +
-  labs(x='Days since immunization', y=NULL)  + theme_grey()+
-  facet_wrap(~EPS)
+  guides(fill='none') +
+  facet_wrap(~EPS, nrow = 3, labeller = label_parsed) + myTheme
   
 
 
-plot_df <- out_df %>%
+plot_df2 <- out_df %>%
   select(timeseries, MZ.1, MZ.2, EPS) %>%
   gather(-c(timeseries, EPS), key = "popl", value = "counts") 
 
-aresf <- plot_df %>%
+aresf <- plot_df2 %>%
   group_by(timeseries, EPS) %>%
   mutate(n_tot = sum(counts), 
          percentage = counts/n_tot)
+
+aresf$EPS <- factor(aresf$EPS, levels = c("0.01", "0.7", "1", "3"), ordered = TRUE,
+                      labels = c(expression(paste(phi, "=0.01")), expression(paste(phi, "=0.7")),
+                                 expression(paste(phi, "=1")), expression(paste(phi, "=3"))))
 
 # Give a specific order:
 aresf$popln <- factor(aresf$popl , levels=c("MZ 45.1", "MZ 45.2") )
@@ -156,122 +176,17 @@ aresf$popln <- factor(aresf$popl , levels=c("MZ 45.1", "MZ 45.2") )
 ggplot(data = aresf, aes(x= (timeseries), y=percentage*100, fill=popl)) +
   geom_area(alpha=0.6, colour="white") +
   scale_fill_manual(values = c(4,2))+
-  theme_ipsum() +
   scale_x_log10(limits = c(7, 60), breaks=c(7, 14, 28, 56)) +
   #xlim(7, 56) + #scale_y_log10() + 
   labs(x='Days since immunization', y=NULL)  + 
-  facet_wrap(.~EPS, labeller = as_labeller(plot_labl)) +
-  theme(legend.title = element_blank())
+  facet_wrap(.~EPS, labeller = label_parsed) +
+  guides(fill='none') + 
+  theme(legend.title = element_blank()) + myTheme
 
 
 
 
 
 
-
-#############
-stop()
-
-boot_func <- function(eps){
-  ## Sampling random numbers to select the row from posterior dist matrix
-  set.seed(1456)
-  rowvec <- sample(1:nrow(fit_ss), 300, replace = FALSE) %>% sort()
-  rowvec <- 
-  
-  ## initiating empty matrices to store the output
-  Fo_df <- matrix(nrow = length(rowvec), ncol = length(ts_pred))
-  GC_df <- matrix(nrow = length(rowvec), ncol = length(ts_pred))
-  MZ_df <- matrix(nrow = length(rowvec), ncol = length(ts_pred))
-  
-  ## run the model with different sets of parameters sampled from the posterior dist matrix
-  for (i in 1:length(rowvec)) {
-    ## ith row from posterior dist matrix
-    parms_vec <- c(fit_ss[rowvec[i], ], "epsilon" = eps)  ## epsilon denotes the loss of Activated FO B cells in addition to their conversion to GC and MZ
-    ### simulation using the selected params
-    sol_ode <- data.frame(ode(y= state, times=ts_pred, func = ode_func, parms = parms_vec))
-    
-    ### outpur
-    Fo_df[i, ] <- sol_ode$Y1
-    GC_df[i, ] <- sol_ode$Y2
-    MZ_df[i, ] <- sol_ode$Y3
-  }
-  
-  Fo_summary <- data.frame(Fo_df) %>%
-    gather(factor_key = TRUE) %>%
-    group_by(key) %>%
-    summarize(lb = quantile(value, probs = 0.045),
-              median = quantile(value, probs = 0.5),
-              ub = quantile(value, probs = 0.955)) %>%
-    bind_cols("timeseries" = ts_pred) %>%
-    mutate(key = "FoB")
-  
-  GC_summary <- data.frame(GC_df) %>%
-    gather(factor_key = TRUE) %>%
-    group_by(key) %>%
-    summarize(lb = quantile(value, probs = 0.045),
-              median = quantile(value, probs = 0.5),
-              ub = quantile(value, probs = 0.955)) %>%
-    bind_cols("timeseries" = ts_pred) %>%
-    mutate(key = "GCB")
-  
-  MZ_summary <- data.frame(MZ_df) %>%
-    gather(factor_key = TRUE) %>%
-    group_by(key) %>%
-    summarize(lb = quantile(value, probs = 0.045),
-              median = quantile(value, probs = 0.5),
-              ub = quantile(value, probs = 0.955)) %>%
-    bind_cols("timeseries" = ts_pred) %>%
-    mutate(key = "MZB")
-  
-  
-  rbind(Fo_summary, GC_summary, MZ_summary)%>%
-    filter(timeseries >= 5)
-}
-
-pooled_df <- boot_func(0.01)
-  
-ggplot(pooled_df) +
-  geom_line(aes(x=timeseries, y=median, col=key))+
-  geom_ribbon(aes(x = timeseries, ymin = lb, ymax = ub, fill=key), alpha = 0.25) +
-  scale_x_log10() + scale_y_log10(limits=c(1, 1e5))
-
-
-sol_df <- pooled_df %>%
-  select(-lb, -ub) %>%
-  spread(key = key, value = median)
-  
-
-
-ggplot(data = sol_df, aes(x= timeseries)) +
-  geom_area(aes(y= FoB+ GCB + MZB, fill="FO_counts" ), alpha=0.6 , linewidth=.5, colour="white") +
-  geom_area(aes(y= GCB + MZB, fill="GC_counts"), alpha=0.6 , linewidth=.5, colour="white") +
-  geom_area(aes(y= MZB, fill="CARMZ_WT"), alpha=0.6 , linewidth=.5, colour="white") +
-  #scale_fill_viridis(discrete = T) +
-  #theme_ipsum() + 
-  scale_x_log10(limits = c(5, 35), breaks=c(7, 14, 35)) + scale_y_log10() + 
-  labs(x='Days since immunization', y=NULL)  + 
-  guides(fill="none")
-
-plot_df <- pooled_df %>%
-  filter(key != "FoB") %>%
-  filter(timeseries >= 5)
-
-aresf <- plot_df %>%
-  select(-lb, -ub) %>%
-  group_by(timeseries) %>%
-  mutate(n_tot = sum(median), 
-         percentage = median/n_tot)
-
-# Give a specific order:
-aresf$popln <- factor(aresf$key , levels=c("CARMZ_WT", "GC_counts", "FO_counts") )
-
-ggplot(data = aresf, aes(x= (timeseries), y=percentage*100, fill=key)) +
-  geom_area(alpha=0.6 , linewidth=.5, colour="white") +
-  scale_fill_viridis(discrete = T) +
-  theme_ipsum() + 
-  xlim(5, 35) + #scale_y_log10() + 
-  labs(x='Days since immunization', y=NULL)  + 
-  #facet_wrap(.~popln) + 
-  guides(col="none")
   
 
